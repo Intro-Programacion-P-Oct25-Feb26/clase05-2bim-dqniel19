fbff81e5dca2266aac999287b002acdd13d5f39f
Daniel <danielp4p1@gmail.com>
2026-01-15 11:22:06 -0500

    Commit

---
+++ Ejemplos/src/paquete06/Informe.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete06;
+
+/**
+ *
+ * @author UTPL
+ */
+public class Informe {
+    public static void imprimir (int a, int b, int asumab, int axb) {
+        System.out.printf("La suma de %d + %d es igual a: %d\n"
+                + "La multiplicacion de %d * %d es igual a: %d\n",
+                a,b,
+                asumab,
+                a, b,
+                axb);
+    }
+}
